7277170d87fcf2d3b92c0d36aa317a7f3ea6da30
Nandan Prasad <prasadnandan2595@gmail.com>
2024-06-16 12:57:36 +0530

    Add NotFoundPage and JobsPage to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import AddJobPage from "./pages/AddJobPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -10,6 +11,7 @@ const router = createBrowserRouter(
     <Route index element={<HomePage />} />
     <Route path="/jobs" element={<JobsPage />} />
     <Route path="/add-job" element={<AddJobPage />} />
+    <Route path="*" element={<NotFoundPage />} />
   </Route>
   
 )
